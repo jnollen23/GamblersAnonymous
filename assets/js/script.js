54c198ea4a7aa330018e8ecf94a57b5922c6357a
Jacob Nollen <jacob.nollen@hotmail.com>
2022-12-01 21:31:09 -0500

    adding basic currency tracking

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+
+
+//This is the base version for MVP for currency with a login
+//I am trying to keep everything related to Currency in the Currency JS so dont mess with
+//the currency global variables, use the method GetBalance and SetUser
+function UserLogin(){
+    SetUser('user');
+    var userBalance = GetBalance();
+}
