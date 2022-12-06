7106518c134c72ceacaa8ba49c5c3e6557a8cc65
Victor Yang <vyang9887@gmail.com>
2022-12-05 21:17:53 -0500

    Add commands for card functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,68 @@ import {MDCTabBar} from '@material/tab-bar';
 
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
+function createDeck() {
+    $.ajax({
+        url: "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + $('.deckSize').val(),
+        type: 'GET'
+    })
+    .then(function(response) {
+        var deckId = $('.deckId');
+        deckId.attr('value', response.deck_id);
+        $('.createDeck').hide();
+        $('.dealGame').show();
+    });
+}
+
+function drawCard(hand) {
+    $.ajax({
+        url: "https://www.deckofcardsapi.com/api/deck/" + $('.deckId').val() + "/draw/?count=1",
+        type: 'GET'
+    })
+    .then(function(response){
+            $(hand).append($('<img>').attr('src', response.cards[0].image));
+    });
+}
+
+function dealCards() {
+    $('.playerHand').append($('<h2>').text("Player Hand"));
+    $('.dealerHand').append($('<h2>').text("Dealer Hand"));
+
+    $.ajax({
+        url: "https://www.deckofcardsapi.com/api/deck/" + $('.deckId').val() + "/draw/?count=4",
+        type: 'GET'
+    })
+    .then(function(response){
+        response.cards[0].forEach((i, cards) => {
+            if(i < 2) {
+                $('.playerHand').append($('<img>').attr('src', cards.image));
+            }
+            else {
+                $('.dealerHand').append($('<img>').attr('src', cards.image));
+            }
+        })
+    });
+
+    for(var i=0; i < 4; i++){
+        if(i < 2) {
+            playerDraw();
+        }
+        else {
+            dealerDraw();
+        }
+    }
+    $('#drawBtn').hide();
+    $('#playerDrawBtn').show();
+    $('#dealerDrawBtn').show();
+}
+
+function playerDraw() {
+    drawCard('.playerHand');
+}
+
+function dealerDraw() {
+    drawCard('.dealerHand');
+}
 //This is the base version for MVP for currency with a login
 //I am trying to keep everything related to Currency in the Currency JS so dont mess with
 //the currency global variables, use the method GetBalance and SetUser
