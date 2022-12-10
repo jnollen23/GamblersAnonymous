2bc3b7af0150d85d1ccb2ee54b53e0a8182335e1
Arnasia Jackson <arnasial.jackson@gmail.com>
2022-12-10 11:22:34 -0500

    saving changes with materialize added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
-import {MDCTabBar} from '@material/tab-bar';
-
-const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+function openPage(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
 
 //This is the base version for MVP for currency with a login
 //I am trying to keep everything related to Currency in the Currency JS so dont mess with
