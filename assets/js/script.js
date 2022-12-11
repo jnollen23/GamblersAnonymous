96c95713b0e1ae9f466441874ff4f0c20f76b786
Arnasia Jackson <arnasial.jackson@gmail.com>
2022-12-11 13:17:27 -0500

    snake game added and arcade game tab updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function(){
   M.Tabs.init(myTabs,{});
 })
 
+
+
 //This is the base version for MVP for currency with a login
 //I am trying to keep everything related to Currency in the Currency JS so dont mess with
 //the currency global variables, use the method GetBalance and SetUser
